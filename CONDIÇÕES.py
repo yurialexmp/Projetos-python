7041f7f1b53ecd25e34189a369a6e77013ba7f33
Yuri Moreira/ Dev <yuri.alexsandermp@gmail.com>
2025-10-02 11:34:23 -0300

    6

---
+++ CONDIÇÕES.py
@@ -0,0 +1,8 @@
+n1=float(input('digite  a primeira nota do aluno:'))
+n2=float(input('digite a segunda nota do aluno:'))
+m=(n1+n2)/2
+print('Sua média foi:{}'.format(m))
+if m>=6.0:
+    print('Sua média foi boa')
+else:
+    print('Sua média foi ruim')
